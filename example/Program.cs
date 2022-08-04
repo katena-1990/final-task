f2651f25c188dcb6032aa93e5ed1fd38cb7741cd
Katya Panko <katena-1990@mail.ru>
2022-08-04 20:51:43 +0300

    условие и решение

--- example/Program.cs
+++ example/Program.cs
@@ -2,3 +2,13 @@
 Console.WriteLine("Введите массив строк через пробел: ");
 string stroka = Console.ReadLine();
 M = stroka.Split(' ');
+var result = new string[M.Length];
+var size = 0;
+foreach (var value in M)
+{
+if (value.Length<=3)
+{
+    result[size] = value;
+    size++;
+}
+}
